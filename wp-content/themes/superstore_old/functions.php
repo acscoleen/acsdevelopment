2413f94bd25c39b5a2bc139c0cf155b12f935661
Coleen Sumabong <coleen@acshomeandwork.com>
2014-11-13 13:07:01 +0800

    Fixed Functions error with }

--- wp-content/themes/superstore_old/functions.php
+++ wp-content/themes/superstore_old/functions.php
@@ -85,11 +85,10 @@ add_action('woocommerce_after_shop_loop_item_title','woocommerce_template_single
 /*-----------------------------------------------------------------------------------*/
 ?>
 
-function yit_print_custom_style() {
-  ?>
+<?php function yit_print_custom_style() {
   <style type="text/css">
   #megaMenu ul ul.sub-menu { z-index: 99999; }
   </style>
-  <?php
 }
 add_action( 'wp_head', 'yit_print_custom_style' );
+?>
